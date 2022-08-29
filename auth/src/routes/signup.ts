fbbfc727a7cf8c96fe1930eab13649a8df009c89
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-08-29 20:10:10 +0600

    Added signup route implementation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signup',
+  ...[
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating a user...');
+
+    return res.send({});
+  },
+);
 
 export { router as signupRouter };

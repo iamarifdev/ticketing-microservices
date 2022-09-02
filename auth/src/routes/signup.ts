1526ece230c8dcd6baca3c54c1aaa4b5bec83025
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-02 18:24:32 +0600

    Added request validator middleware and implemented signin route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { RequestValidationError } from '../errors';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares';
 import { User } from '../models/user';
 
 const router = express.Router();
@@ -14,13 +14,8 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
   ],
+  validateRequest,
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });

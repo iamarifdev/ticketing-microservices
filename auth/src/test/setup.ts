03e0c60f449ca80ebe1d71fdae47208d53f65fb2
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-02 23:09:21 +0600

    Setting up test environment and added first test

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,28 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+import { app } from '../app';
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdf';
+  
+  mongo = await MongoMemoryServer.create();
+  const mongoURI = mongo.getUri();
+
+  await mongoose.connect(mongoURI, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});

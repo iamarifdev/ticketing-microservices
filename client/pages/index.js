9094784ebe922a8ef888874dcc58c2c862850bea
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-03 23:39:17 +0600

    Added custom hook and refactored codebase

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,22 @@
-const LandingPage = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return (
     <div>
       <h1>Landing Page</h1>
     </div>
   );
-}
+};
+
+// LandingPage.getInitialProps = async () => {
+//   const { data } = await axios.get('/api/users/currentuser');
+//   return data;
+// };
 
 export default LandingPage;

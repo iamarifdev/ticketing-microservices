a82ab5ebbca5429d4c4db3af3f6e679d981fa13d
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-04 22:34:27 +0600

    Added api client that will work both in SSR and SPA

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,7 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
 
   return (
     <div>
@@ -14,9 +10,11 @@ const LandingPage = ({ currentUser }) => {
   );
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const { data } = await axios.get('/api/users/currentuser');
-//   return data;
-// };
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
 
 export default LandingPage;

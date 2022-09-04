4841c73cf33222312f8c161765e7190253e09cbc
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-04 22:40:39 +0600

    Added signin form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,7 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  );
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {

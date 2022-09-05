88d7d04990507473c6f8657bba4d28dc401ae04e
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-05 22:53:09 +0600

    Added ticket service

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import 'express-async-errors';
+
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+
+import { NotFoundError, errorHandler } from '@arifdev.tickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

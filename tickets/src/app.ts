c361b77a8ec76c73c018f6bbb1658711ecba516c
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-05 23:43:13 +0600

    Added TDD based tests for new route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,7 +4,9 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 
-import { NotFoundError, errorHandler } from '@arifdev.tickets/common';
+import { currentUser, errorHandler, NotFoundError } from '@arifdev.tickets/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +18,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   }),
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,18 +1,16 @@
+import jwt from 'jsonwebtoken';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-
-import { app } from '../app';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdf';
-  
+
   mongo = await MongoMemoryServer.create();
   const mongoURI = mongo.getUri();
 
@@ -32,19 +30,16 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  const payload = {
+    id: 'dsfs34534543',
+    email: 'test@test.com',
+  };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password
-    })
-    .expect(201);
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  const cookie = response.get('Set-Cookie');
-
-  return cookie;
+  return [`session=${base64}`];
 };

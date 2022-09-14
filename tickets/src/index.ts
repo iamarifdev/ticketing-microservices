6a1cfbb7f8b02f7fc0a94979b9caaa5c2dcbc786
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-15 00:06:32 +0600

    Created singletone nats client using NatsWrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,6 +12,14 @@ const start = async () => {
   }
   
   try {
+    await natsWrapper.connect('ticketing', 'asdf', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {

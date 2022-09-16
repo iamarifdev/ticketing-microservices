5ccb40202f18b8d3c59ba6b9d6fea9a55229c7a7
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-16 23:22:08 +0600

    Added ticket and order model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+
+import { requireAuth, validateRequest } from '@arifdev.tickets/common';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  ...[
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  },
+);
 
 export { router as newOrderRouter };

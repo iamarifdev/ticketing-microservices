63dbfe8b94ab7acaf12bbccdd18f7a6928515206
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-22 23:50:21 +0600

    Added custom static method to tickets model

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -20,6 +20,7 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: { id: string, version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -43,6 +44,13 @@ const ticketSchema = new mongoose.Schema({
 
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = (event: { id: string, version: number }) => {
+  return Ticket.findOne({ 
+    _id: event.id, 
+    version: event.version - 1 
+  });
+};
 
 ticketSchema.statics.build = ({id, ...attrs}: TicketAttrs) => {
   return new Ticket({

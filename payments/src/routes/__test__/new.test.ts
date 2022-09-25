3746893c6076c5093546ec3b0895f3e896129e64
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-25 17:38:45 +0600

    Added stripe charge implementation and mocked test cases

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import request from 'supertest';
 
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -55,4 +58,33 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = global.generateId();
+
+  const order = Order.build({
+    id: global.generateId(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual('usd');
+  
+  expect(stripe.charges.create).toHaveBeenCalled();
 });

4dd0b3cfd63b36732e634983d123cb6a2d962558
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-25 15:03:40 +0600

    Setting up stripe and update k8s infra env variables

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,18 +1,24 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
-import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@arifdev.tickets/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@arifdev.tickets/common';
+
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
 router.post(
   '/api/payments',
   requireAuth,
-  ...[
-    body('token').not().isEmpty(),
-    body('orderId').not().isEmpty(),
-  ],
+  ...[body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
@@ -29,7 +35,14 @@ router.post(
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
 
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
     res.send({ success: true });
-  });
+  },
+);
 
 export { router as createChargeRouter };

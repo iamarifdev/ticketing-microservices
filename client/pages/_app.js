1f62779297bfb1276ba6ca32f99fa7eb542ebb50
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-26 13:08:29 +0600

    Refactored client app and index implementation for child component's getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,7 +7,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
@@ -18,7 +18,7 @@ AppComponent.getInitialProps = async (appContext) => {
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
   }
 
   return { pageProps, ...data };

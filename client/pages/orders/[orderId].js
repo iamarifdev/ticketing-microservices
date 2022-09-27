9e266e25699abbf8a65006e6b10d258e3b020a47
Md. Ariful Islam <arifjmamun24@gmail.com>
2022-09-27 23:26:37 +0600

    Client: Integrated stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -21,7 +22,17 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft}</div>;
+  return (
+  <div>
+    Time left to pay: {timeLeft}
+    <StripeCheckout
+      token={(token) => console.log(token)}
+      stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      amount={order.ticket.price * 100}
+      email={currentUser.email}
+    />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
